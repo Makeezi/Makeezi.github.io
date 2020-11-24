f7f77bf81ae64577464596f32a727fc84b0e08f6
Heinonen Markus Antero <markus.heinonen@edu.tampere.fi>
2020-11-24 15:16:07 +0200

    fanikauppa add

---
+++ fanikauppa/precache-manifest.ba0945b69a28db7d0f535ec7f9fab3a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67802e7c96f90d614c342e26d6a1558",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "4b11b42cec9f4a18f03e",
+    "url": "/fanikauppa/static/css/main.09a5ef38.chunk.css"
+  },
+  {
+    "revision": "d119745731e880b35a56",
+    "url": "/fanikauppa/static/js/2.59d5e86e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.59d5e86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b11b42cec9f4a18f03e",
+    "url": "/fanikauppa/static/js/main.d3120b20.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  }
+]);
